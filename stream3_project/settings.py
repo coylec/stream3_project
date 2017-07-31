d8eb603fb0b66790fb033137e3428ef52950dd3b
Conrad Coyle <coylec@users.noreply.github.com>
2017-07-31 18:53:00 +0100

    Added djangobower & gitignore

--- stream3_project/settings.py
+++ stream3_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangobower',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_FINDER = ('djangobower.finders.BowerFinder',)
+
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, 'components')
+
+# BOWER_PATH = '/user/bin/bower'
+
+BOWER_INSTALLED_APPS = (
+    'bootstrap',
+    'jquery',
+)
 
 # TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
