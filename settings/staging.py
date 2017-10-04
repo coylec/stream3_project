9016482582dfc5a17694dfe02f288df41b94ad13
Conrad Coyle <coylec@users.noreply.github.com>
2017-10-04 16:30:42 +0100

    Update database settings

--- settings/staging.py
+++ settings/staging.py
@@ -1,13 +1,11 @@
 from base import *
+import dj_database_url
 
 
 DEBUG = False
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 SITE_URL = 'https://coylec-streamthree-project.herokuapp.com'
