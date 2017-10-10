2bc93687751da862f8e5d23cf55f3c3937a6ab0b
Conrad Coyle <coylec@users.noreply.github.com>
2017-10-10 17:01:45 +0100

    Remove map function from doc.ready

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,17 @@
+function initMap() {
+    var finea = {lat: 53.779720, lng: -7.385282};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: finea
+    });
+    var marker = new google.maps.Marker({
+        position: finea,
+        map: map
+    });
+}
+
+
 $(document).ready(function() {
 
 	var offset = 10;
@@ -42,18 +56,5 @@ $('.logout-confirm').confirm({
         }
     }
 });
-
-function initMap() {
-    var finea = {lat: 53.779720, lng: -7.385282};
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: finea
-    });
-    var marker = new google.maps.Marker({
-        position: finea,
-        map: map
-    });
-}
 
 });
